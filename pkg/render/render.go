4b672584391cb5be9bcce0468a4286bcfcb6200c
Ali Noureddine <ali-nd2001@outlook.com>
2023-05-13 02:56:21 -0400

    Added a caching function for templates

--- pkg/render/render.go
+++ pkg/render/render.go
@@ -2,22 +2,61 @@ package render
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 	"text/template"
 )
 
 // RenderTemplate renders templates using html/template
-func RenderTemplate(w http.ResponseWriter, tmpl string) {
-	parsedTemplate, err := template.ParseFiles("./templates/"+tmpl, "./templates/base.layout.tmpl")
+func RenderTemplateTest(w http.ResponseWriter, tmpl string) {
+	parsedTemplate, _ := template.ParseFiles("./templates/"+tmpl, "./templates/base.layout.tmpl")
+	err := parsedTemplate.Execute(w, nil)
 	if err != nil {
 		fmt.Println("Error parsing template:", err)
 		http.Error(w, "Internal server error", http.StatusInternalServerError)
-		return
 	}
-	err = parsedTemplate.Execute(w, nil)
+
+}
+
+var tc = make(map[string]*template.Template)
+
+func RenderTemplate(w http.ResponseWriter, t string) {
+	var tmpl *template.Template
+	var err error
+
+	// check if the template is in the cache
+	_, ok := tc[t]
+
+	if !ok {
+		// if not, create the template cache
+		err = createTemplateCache(t)
+		if err != nil {
+			fmt.Println("Error parsing template:", err)
+		}
+
+	} else {
+		// get the template from the cache
+		log.Println("Template from cache")
+	}
+	tmpl = tc[t]
+	err = tmpl.Execute(w, nil)
 	if err != nil {
-		fmt.Println("Error executing template:", err)
-		http.Error(w, "Internal server error", http.StatusInternalServerError)
-		return
+		fmt.Println("Error parsing template:", err)
 	}
+}
+
+func createTemplateCache(t string) error {
+	templates := []string{
+		fmt.Sprintf("./templates/%s", t),
+		"./templates/base.layout.tmpl",
+	}
+	// parse the template
+	tmpl, err := template.ParseFiles(templates...)
+	if err != nil {
+		return err
+	}
+	// cache the template
+	tc[t] = tmpl
+	return nil
+
 }
