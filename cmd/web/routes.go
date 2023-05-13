a48f6889f66ee127af34d6dd3824950dc2812c6d
Ali Noureddine <ali-nd2001@outlook.com>
2023-05-13 16:24:29 -0400

    Added Recoverer to prevent panic

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -7,12 +7,16 @@ import (
 	"github.com/Alinoureddine1/ZenStay/pkg/handlers"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func routes(app *config.AppConfig) http.Handler {
 	//http handler
 
 	mux := chi.NewRouter()
+
+	//Recovers instead of panicking
+	mux.Use(middleware.Recoverer)
 
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
