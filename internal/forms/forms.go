dd48d2a7fe9cadce534d2383162a7a89889df64b
Ali Noureddine <ali-nd2001@outlook.com>
2023-05-25 22:07:58 -0400

    Added minlength function

--- internal/forms/forms.go
+++ internal/forms/forms.go
@@ -31,3 +31,14 @@ func (f *Form) Has(field string, r *http.Request) bool {
 	}
 	return true
 }
+
+// MinLength checks for string minimum length
+func (f *Form) MinLength(field string, r *http.Request, min int) bool {
+
+	x := r.Form.Get(field)
+	if len(x) < min {
+		f.Errors.Add(field, "This field must be at least "+string(min)+" characters long")
+		return false
+	}
+	return true
+}
