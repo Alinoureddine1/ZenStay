493f96c2e72ea89b687ac9c42ec79789ebd4dc99
Ali Noureddine <ali-nd2001@outlook.com>
2023-06-10 14:03:34 -0400

    Added Tests for middleware

--- cmd/web/middleware_test.go
+++ cmd/web/middleware_test.go
@@ -18,3 +18,17 @@ func TestNoSurf(t *testing.T) {
 		t.Errorf("type is not http.Handler, but is %T", v)
 	}
 }
+
+func TestSessionLoad(t *testing.T) {
+
+	var myH myHandler
+
+	h := SessionLoad(&myH)
+
+	switch v := h.(type) {
+	case http.Handler:
+		// do nothing
+	default:
+		t.Errorf("type is not http.Handler, but is %T", v)
+	}
+}
